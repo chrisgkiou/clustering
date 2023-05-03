ce346418f189b729830cb22f36072f7222ce3a83
christos gkiourtzidis <xristosgkiou@gmail.com>
2023-05-03 15:32:00 +0300

    x

--- checkidentity.py
+++ checkidentity.py
@@ -1,19 +1,10 @@
-import hashlib
+hex_hash = "695e69354dbd7be3ef899ac749837943ed73f7f8f7d7dd5f179ce5fe5eecbe6b"
+binary_hash = bin(int(hex_hash, 16))[2:].zfill(256)
+print(binary_hash)
 
-# Binary representation of the hash
-binary_hash = '0110100101011110011010010011010101001101101111010111101111100011111011111000100110011010110001110100100110000011011110010100001111101101011100111111011111111000111101111101011111011101010111110001011110011100111001011111111001011110111011001011111001101011'
+test_binary_hash = "0110100101011110011010010011010101001101101111010111101111100011111011111000100110011010110001110100100110000011011110010100001111101101011100111111011111111000111101111101011111011101010111110001011110011100111001011111111001011110111011001011111001101011"
 
-# Convert binary to bytes
-byte_hash = int(binary_hash, 2).to_bytes((len(binary_hash) + 7) // 8, byteorder='big')
-
-# Compute hash using SHA256
-computed_hash = hashlib.sha256(byte_hash).hexdigest()
-
-# Given hash
-given_hash = '695e69354dbd7be3ef899ac749837943ed73f7f8f7d7dd5f179ce5fe5eecbe6b'
-
-# Compare the computed hash with the given hash
-if computed_hash == given_hash:
-    print("The binary representation corresponds to the given hash")
+if binary_hash == test_binary_hash:
+    print("The binary representation corresponds to the given hash.")
 else:
-    print("The binary representation does not correspond to the given hash")
+    print("The binary representation does not correspond to the given hash.")
